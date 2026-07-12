a3c1aec3619c9c90ee1ea7d000f164b40e1c7266
Zachary V <z.volpi@hotmail.com>
2026-07-12 22:34:58 +0200

    Package updates (#5)
    * feat: use catalog definition to regroup version
    
    * feat: extract common lib in a catalog
    
    * feat: migrate to tsdown
    
    * feat: migrate to ts 7+
    
    * feat: normalize vite
    
    * use specific version
    
    * bump pnpm
    
    * keep 10.30 pnpm version
    
    * lint fix
    
    * feat: add change setup

--- packages/plugin-prometheus/tsdown.config.ts
+++ packages/plugin-prometheus/tsdown.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
-  treeshake: true,
   minify: false,
   target: "es2022",
   outDir: "dist",
